145d126ff0c0b9e965554c5f5159dd0ee8bea46c
Moriya Hiroyuki <Hiroyuki.Moriya@Gmail.com>
2025-09-07 12:40:44 +0900

    完成
    LEDがつくように改造した。詳細はDocumentフォルダ参照

--- sw_count/src/main.cpp
+++ sw_count/src/main.cpp
@@ -2,25 +2,24 @@
 
 // sketch name = sw_count
 // スイッチの状態（押されているかどうか）を表示する
+// 参考 chapter5-2 P113
 
 int SWITCH_SOCKET = 2; // スイッチの接続ピン番号
+int count = 0; // スイッチが押された回数をカウントする変数  
 
 void setup() {
   Serial.begin(9600);
-  pinMode(SWITCH_SOCKET, INPUT_PULLUP); // スイッチ（PD2）を内部プルアップ入力に設定
+  pinMode(SWITCH_SOCKET, INPUT); // スイッチ（PD2）を入力に設定
 }
 
 void loop() {
-
-  Serial.println( digitalRead( SWITCH_SOCKET ) ); // スイッチの状態をシリアルモニタに出力
-
-  delay(3000); // 3秒待機
+  if ( digitalRead( SWITCH_SOCKET ) == 1) { // スイッチが押されたら
+    count = count + 1;                      // カウントを1増やす  
+    Serial.print("Count:");
+    Serial.println(count);                  // カウントを表示
+    while ( digitalRead ( SWITCH_SOCKET ) == 1){ // スイッチが押されている間は
+      delay(100);                          // 100ms待つ (チャタリング対策)
+    }
+  }
+  // スイッチが押されていないときは何もしない
 }
-
-//void loop() {
-////  タクトスイッチを押している状態を読み取り、シリアルモニタに表示する
-////  押されていれば 1、押されていなければ 0 を表示
-//  Serial.println( digitalRead( SWITCH_SOCKET ) ); // カウントをシリアルモニタに出力
-//
-//  delay(3000); // 3秒待機
-//}
